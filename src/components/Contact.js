6b28a4a69cd73f5af9977cf17661d6ca472ee82b
sjc274 <jsa356@sfu.ca>
2024-08-15 23:58:10 -0700

    contact form working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import Swal from 'sweetalert2';
 import contactImg from '../assets/img/contact-img.svg';
 
 export const Contact = () => {
     const formInitialDetails = {
-        firstName: '',
-        lastName: '',
+        fullName: '',
         email: '',
         phone: '',
         message: ''
@@ -22,26 +22,42 @@ export const Contact = () => {
         });
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const onSubmit = async (event) => {
+        event.preventDefault();
         setButtonText('Sending...');
-        let response = await fetch('https://localhost:5000', {
-            method: 'POST',
-            headers: {
-                'Content-Type': "Application/json;charset=utf-8"
-            },
-            body: JSON.stringify(formDetails),
+    
+        const formData = new FormData(event.target);
+        formData.append("access_key", "832497b9-b0cd-4da4-95c1-a63d69f290ec");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
         });
-        setButtonText('Send');
-        let result = await response.json();
-        setFormDetails(formInitialDetails);
-        if (result.code === 200) {
-            setStatus({ success: true, message: 'Message sent successfully!' });
+
+        const data = await response.json();
+
+        if (data.success) {
+            Swal.fire({
+                title: "Success!",
+                text: "We received your message!",
+                icon: "success",
+                confirmButtonText: "Cool",
+            });
+            setButtonText('Send');
+            event.target.reset();
+        } else {
+            Swal.fire({
+                title: "Error!",
+                text: "Something went wrong!",
+                icon: "error",
+                confirmButtonText: "Try Again",
+            });
+            setButtonText('Send');
         }
-        else {
-            setStatus({ success: false, message: 'Something went wrong. Please try again later.' });
-        }
-    }
+    };
 
     return (
         <section className="contact" id="connect">
@@ -52,30 +68,22 @@ export const Contact = () => {
                     </Col>
                     <Col md={6}>
                         <h2>Get In Touch</h2>
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={onSubmit}>
                             <Row>
                                 <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => {
-                                        onFormUpdate('firstName', e.target.value);
+                                    <input required className="name" type="text" value={formDetails.fullName} placeholder="Full Name" onChange={(e) => {
+                                        onFormUpdate('fullName', e.target.value);
                                     }} />
                                 </Col>
+                                
                                 <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => {
-                                        onFormUpdate('lastName', e.target.value);
-                                    }} />
-                                </Col>
-                                <Col sm={6} className="px-1">
-                                    <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => {
+                                    <input required className="email" type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => {
                                         onFormUpdate('email', e.target.value);
                                     }} />
                                 </Col>
-                                <Col sm={6} className="px-1">
-                                    <input type="tel" value={formDetails.phone} placeholder="Phone Number" onChange={(e) => {
-                                        onFormUpdate('phone', e.target.value);
-                                    }} />
-                                </Col>
+                                
                                 <Col>
-                                    <textarea row="6" value={formDetails.message} placeholder="Message" onChange={(e) => {
+                                    <textarea required className="message" row="6" value={formDetails.message} placeholder="Message" onChange={(e) => {
                                         onFormUpdate('message', e.target.value);
                                     }} />
                                     <button type="submit"><span>{buttonText}</span></button>
